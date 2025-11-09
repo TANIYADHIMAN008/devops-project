fb5fd001aab8fa39c25c4fec859cf88cf8192732
Taniya <taniyadhiman738@gamil.com>
2025-11-09 16:43:48 +0000

    Remove duplicate README and makefile files

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int  main(){
-   char name[50];
-   printf("\033[1;36mEnter your name:  \033[0m");
-  scanf("%49s", name);
-printf("\033[1;32mHello, %s! Welcomw to c programming!\033[om\n",name);
-return 0;
-} 
--- project1.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int  main() {
-    printf("Hello, Taniya! This is my DevOps Project running inside Docker.\n");
-return 0;
-}
